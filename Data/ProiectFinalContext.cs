4013becaae2b132372140b3b9d6d424899685688
Alex <Alex@DESKTOP-JSH0I24>
2022-02-17 03:41:33 +0200

    Add project files.

---
+++ Data/ProiectFinalContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProiectFinal.Models;
+
+namespace ProiectFinal.Data
+{
+    public class ProiectFinalContext : DbContext
+    {
+        public ProiectFinalContext (DbContextOptions<ProiectFinalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProiectFinal.Models.Game> Game { get; set; }
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProiectFinal.Models
+{
+    public class Game
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Game Name")]
+        public string GameName { get; set; }
+        public string Company { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+       
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
